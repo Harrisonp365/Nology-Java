b21610f69d6fdd2f650924bd05cef25e35664b3a
Harrisonp365 <harrisonpowell365@gmail.com>
2021-10-27 10:37:01 +1100

    debuggin lesson and loops challenge

--- Arrays&Loops/src/com/nology/ArrayUtils.java
+++ Arrays&Loops/src/com/nology/ArrayUtils.java
@@ -1,5 +1,14 @@
 package com.nology;
 
+//Create a class called ArrayUtils
+//        Create a static method that takes in an int[]
+//        ... and returns a new int[] where all numbers have been incremented
+//        Input => Output Examples:
+//        { 1, 2, 3 } => { 2, 3, 4 }
+//        { 45, 23 } => { 46, 24 }
+//        { } => { }
+
+
 public class ArrayUtils {
 
     public static int[] incriment(int[] arr) {
