e039ec492741b9c22cf9ee5a0bcc80bed2ba0750
Harrisonp365 <harrisonpowell365@gmail.com>
2021-11-08 11:31:42 +1100

    first part of cals lesson showing get requests and get requests with params

---
+++ springboot/iceland/src/main/java/io/nology/iceland/controllers/BookController.java
@@ -0,0 +1,25 @@
+package io.nology.iceland.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/books")
+public class BookController {
+    @GetMapping(value = "/test")
+    public String test() {
+        return "Hello world!";
+    }
+
+    @GetMapping(value = "/test/{name}")
+    public String testName(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping(value = "/test/{name}/{age}")
+    public String testNameAge(@PathVariable String name, @PathVariable int age) {
+        return String.format("Hello %s, you are %d years old", name, age);
+    }
+}
